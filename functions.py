c4ff23317c1ba2db639a16da4fdb556bec774e22
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-04-04 15:03:34 +0300

    this is my git

---
+++ functions.py
@@ -0,0 +1,27 @@
+#This  is the print function
+"""This  is  the  print  function"""
+print("Hello , world!")
+print(2)
+print(3.4)
+
+#Variables
+x=2
+y=3
+print(x)
+print(y)
+print(x+y)
+print(x*y)
+name=input("what  is  your  name?")
+print(name)
+
+first_number=int(input("what  is  your  first"))#fiirst integer
+second_number=int(input("what  is your  second"))#second  integer
+print(first_number*second_number)#first  product
+
+number_one=int(input("what  is  your  first"))#first  integer
+number_two=int(input("what  is your  second"))#second  integer
+print(f"{number_one * number_two}")#second  product
+#This  is  printing  in  float 
+z=float(input("first  name"))#first  float
+r=float(input("second name"))#second  float
+print(z*r)
