7d81e9fb0b8b48078845b2f7b99b6d0ad4330921
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-04-05 22:56:43 +0300

    assignment

---
+++ bank.py
@@ -0,0 +1,17 @@
+
+
+
+# This code prompts the user to enter a greeting and then checks if the input matches any of the
+# specified greetings. If the input matches "Hello" or "Hello, Gloria", it prints "0". If the input
+# matches "How are you doing", it prints "20". Otherwise, it prints "100".
+customer = input("greetings ")
+
+
+if customer == "Hello":
+   print("0")
+elif customer == "Hello, Gloria":
+    print("0")
+elif customer == "How are you doing":
+    print("20")
+else:
+    print("100")
