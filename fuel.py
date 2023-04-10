6b76a850308920b91bfa3736035bd73d29e69f07
Gloria6020 <129518115+Gloria6020@users.noreply.github.com>
2023-04-10 06:30:34 +0300

    assignment

---
+++ fuel.py
@@ -0,0 +1,27 @@
+def main ():
+    final_fuel = get_percent()
+    percent_fuel = round(final_fuel * 100)
+    if percent_fuel <= 1:
+        print("E")
+    elif percent_fuel >= 99:
+        print("F")
+    else:
+        print(f"{percent_fuel}%")
+
+
+def get_percent():
+    try:
+        while True:
+            fraction = input("Fuel Fraction: ")
+            x, y = fraction.split('/')
+            x = int(x)
+            y = int(y)
+            fuel = x/y
+            if fuel <= 1:
+                return fuel
+
+    except (ValueError, ZeroDivisionError):
+        pass
+
+
+main ()
